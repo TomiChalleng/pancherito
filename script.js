84ef9425a564ebba3d6c2eeafbaa761ea657a13a
Tomás <tpbarreras@proton.me>
2025-08-23 18:56:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const boton = document.getElementById("aiagh");
 var c = 0;
 var i = 0;
+var contador = 0;
+var restante = 60;
 
 boton.addEventListener("click", ()=> {
     console.log("DEJEN DE CLICKEAR BOTONES! ! !");
     c++;
+    contador++;
+    restante --;
+    boton.innerText = `Te quedam ${restante} clicks! !!  APURATE`
     if(c > 20) {
         alert("DALE SIGUE PULSANDO");
         c = 0;
+        restante++;
         i++;
     }
     if (i > 2 && confirm("VEN TE LLEVO A UN LUGAR SEGURO")) {
-        window.location.href = "https://www.youtube.com/@DragonFire666HD?sub_confirmation=1"
+        window.location.href = "https://www.youtube.com/watch?v=IHC097KBAYk?sub_confirmation=1"
     }
 });
 
@@ -23,10 +29,12 @@ function contar() {
         tiempo++;
         tiempoTexto.innerText = `Tiempo SANScurrido: ${tiempo}`
     if (tiempo == 20) {
-        window.location.href = "https://www.youtube.com/@DragonFire666HD?sub_confirmation=1"
+        window.location.href = "https://www.youtube.com/watch?v=IHC097KBAYk?sub_confirmation=1"
         return;
     }        
     }, 1000)
 };
+
+
 
 contar();
